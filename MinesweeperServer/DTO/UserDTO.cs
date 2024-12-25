f270cbc77ad24d247c0eacf37ed79a2a7f25f615
User <User@LAB3-3>
2024-12-25 11:14:55 +0200

    error 500 for some reason?

--- MinesweeperServer/DTO/UserDTO.cs
+++ MinesweeperServer/DTO/UserDTO.cs
@@ -4,7 +4,6 @@ namespace MinesweeperServer.DTO
 {
     public class UserDTO
     {
-        public int? Id { get; set; }
         public string Name { get; set; }
 
         public string Email { get; set; }
